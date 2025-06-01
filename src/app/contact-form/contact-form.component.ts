18b4d845399f07b086a3c33db69a1e53ee75a400
locobuzz-dhanesh <dhanesh.mengineni@locobuzz.com>
2025-06-02 03:56:55 +0530

    Implemented Reactive Forms

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,22 +1,51 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReversePipe } from '../reverse.pipe';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [FormsModule, CommonModule,ReversePipe],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
+  contactForm!: FormGroup ;
   submissionMessage: string = '';
-  submittedName: string = '';
 
-  onSubmit(formData: any) {
-    console.log('Form data submitted:', formData);
-    this.submittedName = formData.name; // Store the submitted name
-    this.submissionMessage = 'Your message has been submitted successfully!';
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Reactive Form submitted:', this.contactForm.value);
+      this.submissionMessage = 'Your message has been submitted successfully (Reactive)!';
+      this.contactForm.reset(); // Reset the form after submission
+    } else {
+      // Trigger validation to display errors
+      Object.values(this.contactForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsTouched();
+          control.updateValueAndValidity();
+        }
+      });
+    }
+  }
+
+  get nameControl() {
+    return this.contactForm.controls['name'];
+  }
+
+  get emailControl() {
+    return this.contactForm.controls['email'];
+  }
+
+  get messageControl() {
+    return this.contactForm.controls['message'];
   }
 }
